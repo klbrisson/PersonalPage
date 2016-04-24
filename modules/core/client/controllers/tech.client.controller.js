c34ce4f3b7b934ef212b052ec558f32546a07ac5
Kelsey Brisson <klbrisson@gmail.com>
2016-04-23 19:45:58 -0600

    Add carousel functionality; Add in new logos; Remove unnecessary files;

--- modules/core/client/controllers/tech.client.controller.js
+++ modules/core/client/controllers/tech.client.controller.js
@@ -5,14 +5,24 @@
     .module('core')
     .controller('WellCtrl', WellController);
 
-  WellController.$inject = ['$scope'];
+  WellController.$inject = ['$scope', '$interval'];
 
-  function WellController($scope) {
-    var imgs = ['html_js_css', 'csharp', 'angularjs', 'd3js', 'grunt', 'mongodb', 'nodejs', 'sass'];
+  function WellController($scope, $interval) {
+    var imgs = ['javascript', 'html-5', 'css-3', 'c-sharp', 'angular-js', 'jquery-logo', 'd3-js', 'grunt-white', 'mongo-db', 'node-js', 'sass'];
     var images = $scope.images = [];
+    var hidden = [];
 
     for (var i = 0; i < imgs.length; i++) {
-      images.push('/modules/core/client/img/logos/' + imgs[i] + '.png');
+      if (i < 5) {
+        images.push('/modules/core/client/img/logos/' + imgs[i] + '.png');
+      } else {
+        hidden.push('/modules/core/client/img/logos/' + imgs[i] + '.png');
+      }
     }
+
+    $interval(function() {
+      images.push(hidden.shift());
+      hidden.push(images.shift());
+    }, 2400);
   }
 }());
