dfec72b76178dbf2490be03c87d4588a737f5792
Kelsey Brisson <klbrisson@gmail.com>
2016-04-28 02:26:27 -0600

    testing mongolabs uri

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: 'mongodb://heroku_l77t9v08:rkvfmh7rp1m130tg2v9ptim55u@ds021681.mlab.com:21681/heroku_l77t9v08',//process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
     options: {
       user: '',
       pass: ''
