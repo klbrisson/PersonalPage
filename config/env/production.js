412c04e90dc26a15f7d12b17f83e5250433f625a
Kelsey Brisson <klbrisson@gmail.com>
2016-04-28 04:35:27 -0600

    update styling to blue/grey geometric; add favicon, change page title

--- config/env/production.js
+++ config/env/production.js
@@ -10,7 +10,7 @@ module.exports = {
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
-    uri: 'mongodb://heroku_l77t9v08:rkvfmh7rp1m130tg2v9ptim55u@ds021681.mlab.com:21681/heroku_l77t9v08', //process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: 'mongodb://heroku_l77t9v08:rkvfmh7rp1m130tg2v9ptim55u@ds021681.mlab.com:21681/heroku_l77t9v08', // process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
       user: '',
       pass: ''
